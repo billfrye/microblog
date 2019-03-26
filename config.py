c0c4131fea4d010cc585d887652930a369370b6f
Bill Frye <3730304+billfrye@users.noreply.github.com>
2019-03-26 12:38:32 -0500

    Testing change.

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+print('basedir', basedir)
+
 class Config(object):
         SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
 
